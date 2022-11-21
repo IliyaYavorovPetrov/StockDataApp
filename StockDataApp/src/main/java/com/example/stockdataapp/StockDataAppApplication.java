14255a11e49a98888ed80f6382e41a74a051801e
I.P <72142537+IliyaYavorovPetrov@users.noreply.github.com>
2022-11-21 22:37:13 +0200

    temp

---
+++ StockDataApp/src/main/java/com/example/stockdataapp/StockDataAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockdataapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockDataAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockDataAppApplication.class, args);
+    }
+
+}
---
+++ StockDataApp/src/test/java/com/example/stockdataapp/StockDataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockdataapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDataAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
